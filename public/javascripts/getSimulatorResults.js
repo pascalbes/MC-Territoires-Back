bb62c694416361607749ce4341ec3e2b953e91e5
Pascal Besson <pascalbes@gmail.com>
2020-11-27 14:37:25 +0100

    getSimResults optim

--- public/javascripts/getSimulatorResults.js
+++ public/javascripts/getSimulatorResults.js
@@ -1,11 +1,21 @@
-const getAreaInfos = require('./getAreaInfos')
-const getLineChartInfos = require('./getLineChartInfos')
-const getPieInfos = require('./getPieInfos')
 const getJauge = require('./getJauge')
-const getImpacts = require('./getImpacts')
 const getCompoChartInfos = require('./getCompoChartInfos')
 
+function setCompoObject(rows, i, j, k, l) {
 
+    var compoTemp = getCompoChartInfos(rows,i,0);
+
+    return {
+        graphData: compoTemp,
+        subtitle: rows[j][1],
+        graphText: rows[k][1],
+        source: rows[l][1],
+        graphType: "CompoChart",
+        title: compoTemp.data.title,
+        legendData: compoTemp.graphDatas
+    }
+
+}
 
 function getSimulatorResults(rows) {
 
@@ -43,16 +53,6 @@ function getSimulatorResults(rows) {
     graphs.climate = getCompoChartInfos(rows,13,0)
     graphs.energy = getCompoChartInfos(rows,21,0)
 
-    // <ChartContainer
-    //       title={results.emiFrance.total.graph.data.title}
-    //       subtitle={results.emiFrance.total.subtitle}
-    //       graphData={results.emiFrance.total.graph}
-    //       graphType="CompoChart"
-    //       graphText={results.emiFrance.total.text}
-    //       legendData={results.emiFrance.total.graph.graphDatas}
-    //       sourceData={results.emiFrance.total.source}
-    //     />
-
     var completeResults = {
         emissions : {
             title: rows[37][0],
@@ -86,298 +86,23 @@ function getSimulatorResults(rows) {
         }
     };
 
-    completeResults.emissions.graphs[0]={
-        graphData: getCompoChartInfos(rows,39,0),
-        subtitle: rows[45][1],
-        graphText: rows[46][1],
-        source: rows[47][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,39,0).data.title,
-        legendData: getCompoChartInfos(rows,39,0).graphDatas
-    }
+    completeResults.emissions.graphs[0]=setCompoObject(rows,39,45,46,47);
+    completeResults.emissions.graphs[1]=setCompoObject(rows,50,53,54,55);
+    completeResults.energieFinale.graphs[0]=setCompoObject(rows,61,67,68,69);
+    completeResults.energieRenouvelable.graphs[0]=setCompoObject(rows,75,80,81,82);
+    completeResults.energieFacture.graphs[0]=setCompoObject(rows,89,103,104,105);
+    completeResults.polluants.graphs[0]=setCompoObject(rows,112,118,119,120);
+    completeResults.polluants.graphs[1]=setCompoObject(rows,123,129,130,131);
+    completeResults.polluants.graphs[2]=setCompoObject(rows,134,140,141,142);
+    completeResults.polluants.graphs[3]=setCompoObject(rows,145,151,152,153);
+    completeResults.polluants.graphs[4]=setCompoObject(rows,156,162,163,164);
 
-    completeResults.emissions.graphs[1]={
-        graphData: getCompoChartInfos(rows,50,0),
-        subtitle: rows[53][1],
-        graphText: rows[54][1],
-        source: rows[55][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,50,0).data.title,
-        legendData: getCompoChartInfos(rows,50,0).graphDatas
-    }
-
-    completeResults.energieFinale.graphs[0]={
-        graphData: getCompoChartInfos(rows,61,0),
-        subtitle: rows[67][1],
-        graphText: rows[68][1],
-        source: rows[69][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,61,0).data.title,
-        legendData: getCompoChartInfos(rows,61,0).graphDatas
-    }
-
-    completeResults.energieRenouvelable.graphs[0]={
-        graphData: getCompoChartInfos(rows,75,0),
-        subtitle: rows[80][1],
-        graphText: rows[81][1],
-        source: rows[82][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,75,0).data.title,
-        legendData: getCompoChartInfos(rows,75,0).graphDatas
-    }
-
-    completeResults.energieFacture.graphs[0]={
-        graphData: getCompoChartInfos(rows,89,0),
-        subtitle: rows[103][1],
-        graphText: rows[104][1],
-        source: rows[105][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,89,0).data.title,
-        legendData: getCompoChartInfos(rows,89,0).graphDatas
-    }
-
-    completeResults.polluants.graphs[0]={
-        graphData: getCompoChartInfos(rows,112,0),
-        subtitle: rows[118][1],
-        graphText: rows[119][1],
-        source: rows[120][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,112,0).data.title,
-        legendData: getCompoChartInfos(rows,112,0).graphDatas
-    }
-    
-    completeResults.polluants.graphs[1]={
-        graphData: getCompoChartInfos(rows,123,0),
-        subtitle: rows[129][1],
-        graphText: rows[130][1],
-        source: rows[131][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,123,0).data.title,
-        legendData: getCompoChartInfos(rows,123,0).graphDatas
-    }
-    
-    completeResults.polluants.graphs[2]={
-        graphData: getCompoChartInfos(rows,134,0),
-        subtitle: rows[140][1],
-        graphText: rows[141][1],
-        source: rows[142][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,134,0).data.title,
-        legendData: getCompoChartInfos(rows,134,0).graphDatas
-    }
-
-    completeResults.polluants.graphs[3]={
-        graphData: getCompoChartInfos(rows,145,0),
-        subtitle: rows[151][1],
-        graphText: rows[152][1],
-        source: rows[153][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,145,0).data.title,
-        legendData: getCompoChartInfos(rows,145,0).graphDatas
-    }
-
-    completeResults.polluants.graphs[4]={
-        graphData: getCompoChartInfos(rows,156,0),
-        subtitle: rows[162][1],
-        graphText: rows[163][1],
-        source: rows[164][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,156,0).data.title,
-        legendData: getCompoChartInfos(rows,156,0).graphDatas
-    }
-
-    completeResults.economie.graphs[0]={
-        graphData: getCompoChartInfos(rows,169,0),
-        subtitle: rows[176][1],
-        graphText: rows[177][1],
-        source: rows[178][1],
-        graphType: "CompoChart",
-        title: getCompoChartInfos(rows,169,0).data.title,
-        legendData: getCompoChartInfos(rows,169,0).graphDatas
-    }
-    
-    
-
-
-    // var emiSecteur = getAreaInfo(rows, 0,0)
-    // var emiSecteurGnl = getAreaInfo(rows, 17,0)
-    
-    
-    // var impacts = getImpacts(rows, 35, 0)// i+1
-    
-    // var emiFrance = {};
-
-    // emiFrance.intro = rows[48][1]
-
-    // emiFrance.total = {
-    //     graph: getCompoChartInfos(rows,50,0),
-    //     subtitle: rows[54][1],
-    //     text: rows[55][1],
-    //     source: rows[56][1]
-    // }
-
-    // emiFrance.sansRupture = {
-    //     graph: getAreaInfo(rows,59,0),
-    //     subtitle: rows[65][1],
-    //     text: rows[66][1],
-    //     source: rows[67][1]
-    // }
-
-    // emiFrance.avecRupture = {
-    //     graph: getAreaInfo(rows,71,0),
-    //     subtitle: rows[77][1],
-    //     text: rows[78][1],
-    //     source: rows[79][1]
-    // }
-
-    // var emiSecteurPie = {
-    //     graph: getPieInfos(rows, 83, 0),
-    //     title: rows[82][1],
-    //     subtitle: rows[98][1],
-    //     text: rows[99][1],
-    //     source: rows[100][1]
-    // } 
-
-    // var dataFrance = {};
-
-    // dataFrance.batiment={}
-
-    // dataFrance.batiment.intro = rows[104][1]
-
-    // dataFrance.batiment.perf = {
-    //     graph: getAreaInfo(rows,106,0),
-    //     subtitle: rows[110][1],
-    //     text: rows[111][1],
-    //     source:rows[112][1]
-    // }
-
-    // dataFrance.batiment.chauffage = {
-    //     graph: getAreaInfo(rows,114,0),
-    //     subtitle: rows[121][1],
-    //     text: rows[122][1],
-    //     source:rows[123][1]
-    // }
-
-
-    // dataFrance.transports={}
-
-    // dataFrance.transports.intro = rows[126][1]
-
-    // dataFrance.transports.distance = {
-    //     graph: getAreaInfo(rows,128,0),
-    //     subtitle: rows[134][1],
-    //     text: rows[135][1],
-    //     source:rows[136][1]
-    // }
-
-    // dataFrance.transports.emissions = {
-    //     graph: getAreaInfo(rows,138,0),
-    //     subtitle: rows[146][1],
-    //     text: rows[147][1],
-    //     source:rows[148][1]
-    // }
-
-
-    // dataFrance.agriculture={}
-
-    // dataFrance.agriculture.intro = rows[152][1]
-
-    // dataFrance.agriculture.parcelles = {
-    //     graph: getAreaInfo(rows,154,0),
-    //     subtitle: rows[158][1],
-    //     text: rows[159][1],
-    //     source:rows[160][1]
-    // }
-
-    // dataFrance.agriculture.emissions = {
-    //     graph: getAreaInfo(rows,163,0),
-    //     subtitle: rows[167][1],
-    //     text: rows[168][1],
-    //     source:rows[169][1]
-    // }
-
-    // dataFrance.conso={}
-
-    // dataFrance.conso.intro = rows[176][1]
-
-    // dataFrance.conso.emissions = {
-    //     graph: getAreaInfo(rows,178,0),
-    //     subtitle: rows[183][1],
-    //     text: rows[184][1],
-    //     source:rows[185][1]
-    // }
-
-    // dataFrance.conso.quantites = {
-    //     graph: getLineChartInfos(rows,187,0),
-    //     subtitle: rows[191][1],
-    //     text: rows[192][1],
-    //     source:rows[193][1]
-    // }
-    
-    // dataFrance.energie={}
-
-    // dataFrance.energie.intro = rows[198][1]
-
-    // dataFrance.energie.facteurs = {
-    //     graph: getLineChartInfos(rows,200,0),
-    //     subtitle: rows[203][1],
-    //     text: rows[204][1],
-    //     source:rows[205][1]
-    // }
-
-    // dataFrance.energie.emissions = {
-    //     graph: getAreaInfo(rows,207,0),
-    //     subtitle: rows[210][1],
-    //     text: rows[211][1],
-    //     source:rows[212][1]
-    // }
-
-    // var emiMonde = {
-    //     intro: rows[218][1],
-    //     total : {
-    //         graph: getLineChartInfos(rows,219,0),
-    //         subtitle: rows[222][1],
-    //         text: rows[223][1],
-    //         source:rows[224][1]
-    //     },
-    //     empreinte : {
-    //         graph: getLineChartInfos(rows,227,0),
-    //         subtitle: rows[230][1],
-    //         text: rows[231][1],
-    //         source:rows[232][1]
-    //     }
-    // }
-
-    // impacts.temperatures = {
-    //     intro: rows[236][1],
-    //     europe: {
-    //         title: rows[238][1], 
-    //         subtitle: rows[239][1], 
-    //         text: rows[240][1],
-    //         source: rows[241][1]
-    //     },
-    //     world: {
-    //         title: rows[243][1],
-    //         subtitle: rows[244][1], 
-    //         text: rows[245][1],
-    //         source: rows[246][1]
-    //     }
-    // }
     
     return {
         indicators: indicators,
         graphs : graphs,
         jaugeDatas: jaugeDatas,
         completeResults: completeResults
-        // emiSecteur: emiSecteur,
-        // emiSecteurGnl: emiSecteurGnl,
-        // emiSecteurPie: emiSecteurPie,
-        // emiFrance : emiFrance,
-        // emiMonde: emiMonde,
-        // // emiParSecteur: emiParSecteur,
-        
-        // impacts: impacts,
-        // dataFrance: dataFrance
     }
 }
 
